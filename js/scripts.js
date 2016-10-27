742ed034297a442caae967ecb14e09062c2e5e96
Cody Brubaker and Margie Nevarez <margie@>
2016-10-27 12:02:47 -0700

    add the next spec to convert 4 digit numbers to roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //business logic
 var ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 var tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+var hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+var thousands = ["", "M", "MM", "MMM"]
 var output = ""
 var tensNumeral = ""
 var romanizer = function(userInput) {
@@ -18,6 +20,25 @@ var romanizer = function(userInput) {
     var num1 = onesFunction(reverseArray[0]);
     var num2 = tensFunction(reverseArray[1]);
     var toRoman = num2 + num1
+    return toRoman
+  } else if ( userInput >= 100 && userInput <= 999) {
+    var convertToString = userInput.toString();
+    var splitToArray = convertToString.split("");
+    var reverseArray = splitToArray.reverse();
+    var num1 = onesFunction(reverseArray[0]);
+    var num2 = tensFunction(reverseArray[1]);
+    var num3 = hundredsFunction(reverseArray[2]);
+    var toRoman = num3 + num2 + num1
+    return toRoman
+  } else if ( userInput >= 1000 && userInput <= 3999) {
+    var convertToString = userInput.toString();
+    var splitToArray = convertToString.split("");
+    var reverseArray = splitToArray.reverse();
+    var num1 = onesFunction(reverseArray[0]);
+    var num2 = tensFunction(reverseArray[1]);
+    var num3 = hundredsFunction(reverseArray[2]);
+    var num4 = thousandsFunction(reverseArray[3]);
+    var toRoman = num4 + num3 + num2 + num1
     return toRoman
   }
   return output
@@ -39,6 +60,22 @@ var tensFunction = function(num2) {
   }
   return output
 }//end of tens function
+
+var hundredsFunction = function(num3) {
+  debugger;
+  for (var index = 0; index < num3; index++) {
+    output = hundreds[num3]
+  }
+  return output
+}//end of hundreds function
+
+var thousandsFunction = function(num4) {
+  debugger;
+  for (var index = 0; index < num4; index++) {
+    output = thousands[num4]
+  }
+  return output
+}//end of thousands function
 
 //user interface
 $(document).ready(function() {
